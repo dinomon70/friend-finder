a4d0d9dc0e18269cac2b6bc7be4bebf98187984e
Dean Kipp <djkipp03@msn.com>
2018-11-16 22:26:43 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friendsArray = [
+    {
+      name: "Ben Dead",
+      
+      scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    },
+    {
+      name: "Taylor Troy",
+      
+      scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+    },
+    {
+      name: "Steve Smith",
+      
+      scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+    },
+    {
+      name: "Roxana Roxana",
+      
+      scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+    },
+    {
+      name: "Jeffrey Justice",
+      
+      scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+    },
+    {
+      name: "Doug Funny",
+      
+      scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+    },
+    {
+      name: "Frank Instine",
+      
+      scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    }
+  ];
+  
+  // export the array. 
+  module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(express.static(__dirname + '/app/data/Images'));
+
+//Routes
+require(path.join(__dirname, "./app/routing/apiRoutes")) (app);
+require(path.join(__dirname, "./app/routing/htmlRoutes")) (app);
+
+
+// LISTENER; "starts" the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
